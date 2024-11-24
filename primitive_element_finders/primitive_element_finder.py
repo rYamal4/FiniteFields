981ff2f32a91638899fd4aabfd9df2bda2a8333c
rashid <pchela25000@gmail.com>
2024-11-24 03:01:15 +0300

    slightly changed dumb finder and added docs

--- primitive_element_finders/primitive_element_finder.py
+++ primitive_element_finders/primitive_element_finder.py
@@ -6,6 +6,22 @@ from utils.logger import logger
 
 
 class DumbPrimitiveElementFinder:
+    """
+    A class to find primitive elements in finite fields.
+
+    Primitive elements are determined using provided object of a
+    AbstractPrimitivePowerFunctions class. The class supports finding
+    either a single primitive element or all primitive elements.
+    After finding primitive elements, they are cached.
+
+    Methods:
+    -------
+    find_any_primitive():
+        Finds and returns any single primitive element.
+    find_all_primitives():
+        Finds and returns all primitive elements.
+    """
+
     def __init__(self,
                  n: int,
                  p: int,
@@ -29,16 +45,16 @@ class DumbPrimitiveElementFinder:
                         flag = False
                         break
                 if flag:
-                    self.__cached_primitives.append(args_list)
-                    return args_list
-
+                    primitive = self.__create_shifted_matrix(args_list)
+                    self.__cached_primitives.append(primitive)
+                    return primitive
 
     def find_all_primitives(self):
         if self.__is_found_all_primitives:
             return self.__cached_primitives
         logger.info("Finding all primitive elements...")
         for args_list in product(range(self.__n), repeat=self.__p):
-            if args_list in self.__cached_primitives:
+            if self.__create_shifted_matrix(args_list) in self.__cached_primitives:
                 continue
             if self.__functions.get()[-1](*args_list) == self.__primitive_pow_zero:
                 flag = True
@@ -47,7 +63,7 @@ class DumbPrimitiveElementFinder:
                         flag = False
                         break
                 if flag:
-                    self.__cached_primitives.append(args_list)
+                    self.__cached_primitives.append(self.__create_shifted_matrix(args_list))
         self.__is_found_all_primitives = True
         return self.__cached_primitives
 
@@ -55,3 +71,12 @@ class DumbPrimitiveElementFinder:
         a = [0] * self.__p
         a[-1] = 1
         return a
+
+    @staticmethod
+    def __create_shifted_matrix(values):
+        size = len(values)
+        identity_matrix = [[1 if i == j else 0 for j in range(size)] for i in range(size)]
+        shifted_matrix = [row[1:] + [row[0]] for row in identity_matrix]
+        for i in range(size):
+            shifted_matrix[i][-1] = values[size - 1 - i]
+        return tuple(tuple(row) for row in shifted_matrix)
