48df674ac7fa286d84d280ed41fd5db55a06efa4
rashid <pchela25000@gmail.com>
2024-11-25 16:55:34 +0300

    refactored dumb finder

--- primitive_element_finders/dumb_primitive_element_finder.py
+++ primitive_element_finders/dumb_primitive_element_finder.py
@@ -1,5 +1,7 @@
 from itertools import product
 
+import numpy as np
+
 from custom_collections.list_set import ListSet
 from primitive_element_finders.abstract_primitive_finder import AbstractPrimitiveFinder
 from primitive_element_finders.dumb_primitive_pow_functions import DumbPrimitivePowFunctions
@@ -15,6 +17,8 @@ class DumbPrimitiveElementFinder(AbstractPrimitiveFinder):
     either a single primitive element or all primitive elements.
     After finding primitive elements, they are cached.
 
+    This implementation is very slow!
+
     Methods:
     -------
     find_any_primitive():
@@ -27,13 +31,14 @@ class DumbPrimitiveElementFinder(AbstractPrimitiveFinder):
         self.__p = p
         self.__n = n
         self.__functions = DumbPrimitivePowFunctions(p, n)
-        self.__cached_primitives = ListSet()
+        self.__cached_primitive: np.ndarray = None
+        self.__cached_primitives: list = None
         self.__primitive_pow_zero = self.__get_primitive_pow_zero()
         self.__is_found_all_primitives = False
 
     def find_any(self):
-        if len(self.__cached_primitives) > 0:
-            return self.__cached_primitives[0]
+        if self.__cached_primitive is not None:
+            return self.__cached_primitive
         logger.info("Finding single primitive element...")
         for args_list in product(range(self.__p), repeat=self.__n):
             if self.__functions.get()[-1](*args_list) == self.__primitive_pow_zero:
@@ -44,16 +49,15 @@ class DumbPrimitiveElementFinder(AbstractPrimitiveFinder):
                         break
                 if flag:
                     primitive = self.__create_shifted_matrix(args_list)
-                    self.__cached_primitives.append(primitive)
-                    return primitive
+                    self.__cached_primitive = np.asarray(primitive)
+                    return self.__cached_primitive
 
     def find_all(self):
         if self.__is_found_all_primitives:
             return self.__cached_primitives
+        self.__cached_primitives = []
         logger.info("Finding all primitive elements...")
         for args_list in product(range(self.__p), repeat=self.__n):
-            if self.__create_shifted_matrix(args_list) in self.__cached_primitives:
-                continue
             if self.__functions.get()[-1](*args_list) == self.__primitive_pow_zero:
                 flag = True
                 for function in self.__functions.get()[:-1]:
@@ -61,7 +65,10 @@ class DumbPrimitiveElementFinder(AbstractPrimitiveFinder):
                         flag = False
                         break
                 if flag:
-                    self.__cached_primitives.append(self.__create_shifted_matrix(args_list))
+                    primitive = self.__create_shifted_matrix(args_list)
+                    if self.__cached_primitive is None:
+                        self.__cached_primitive = np.asarray(primitive, dtype=np.int32)
+                    self.__cached_primitives.append(np.asarray(primitive, dtype=np.int32))
         self.__is_found_all_primitives = True
         return self.__cached_primitives
 
